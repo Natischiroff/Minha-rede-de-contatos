569a457c29c7bccc272b97810d77e00542abbd14
Natalia Schiroff <natalia.schiroff@escola.pr.gov.br>
2025-11-06 21:36:26 +0000

    feffdvd

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react' ;
+import {ScrollView} from 'react-native';
+import Titulo from './componentes/Titulo'
+import Saudacao from './componentes/Saudacao';
+import FotoPerfil from './componentes/FotoPerfil';
+import CartaoUsuario from './componentes/CartaoUsuario';
+
+export default function App() {
+  const contatos = {
+    nome: 'Maria Silva',
+    cargo: 'Desenvolvedora Front-End',
+    imagem: ''
+  }
+}
