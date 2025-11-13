346c19b06176bf4f05d231049afe320476432bb5
Natalia Schiroff <natalia.schiroff@escola.pr.gov.br>
2025-11-13 11:26:42 +0000

    yiyyyi

--- App.js
+++ App.js
@@ -5,17 +5,42 @@ import Saudacao from './componentes/Saudacao';
 import FotoPerfil from './componentes/FotoPerfil';
 import CartaoUsuario from './componentes/CartaoUsuario';
 
-export default function App() {
-  const contatos = {
-    nome: 'Maria Silva',
-    cargo: 'Desenvolvedora Front-End',
-    imagem: ''
-    descricao:''Resolvendo codigos',
-  },
-  {
-    nome:'João Pereira',
-    cargo:'Designer',
-    imagem:''
-    descricao:''
-  }
+export default function App(){
+  return(<ScrollView>
+<Titulo />
+<Saudacao nome= 'Natalia' />
+<FotoPerfil />
+
+
+<CartaoUsuario
+ nome='Emanuelly Campos Heerdt'
+cargo="Desenvolvedora Front-End"
+imagem='perfil01'
+/>
+ 
+ <CartaoUsuario
+ nome='Marilene'
+cargo='Designer UX/UI'
+imagem='perfil01'
+/>
+
+<CartaoUsuario
+ nome='Lucas'
+descricao='Programador Mobile'
+imagem='perfil01'
+/>
+
+<CartaoUsuario
+ nome='Fernando'
+descricao='Programador Mobile'
+imagem='perfil01'
+/>
+
+<CartaoUsuario
+ nome='Luana'
+descricao='Programador Mobile'
+imagem='perfil01'
+/>
+</ScrollView>
+  )
 }
